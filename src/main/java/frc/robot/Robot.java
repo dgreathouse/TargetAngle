68c7a2b10957df61b539cb79de2b6533c58500aa
dgreathouse <darryl.greathouse@gmail.com>
2024-01-29 20:19:57 -0500

    Initial Commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,100 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+public class Robot extends TimedRobot {
+  private Command m_autonomousCommand;
+
+  private RobotContainer m_robotContainer;
+
+  @Override
+  public void robotInit() {
+
+    m_robotContainer = new RobotContainer();
+  }
+
+  @Override
+  public void robotPeriodic() {
+
+    TargetAngle ta = new TargetAngle();
+    // Test 0,0
+    ta.setTargetAngle(0, 0);
+    double ang = ta.getTargetAngle().getDegrees();
+    double actAng = ta.getActualAngle();
+    double hyp = ta.getHyp();
+    // Test 1,0.85
+    ta.setTargetAngle(1, .85);
+    ang = ta.getTargetAngle().getDegrees();
+    actAng = ta.getActualAngle();
+    hyp = ta.getHyp();
+
+    // Test 1,0.1
+    ta.setTargetAngle(1, .1);
+    ang = ta.getTargetAngle().getDegrees();
+    actAng = ta.getActualAngle();
+    hyp = ta.getHyp();
+   // Test 1,0.2
+    ta.setTargetAngle(1, .2);
+    ang = ta.getTargetAngle().getDegrees();
+    actAng = ta.getActualAngle();
+    hyp = ta.getHyp();
+
+
+
+    CommandScheduler.getInstance().run();
+  }
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void disabledExit() {}
+
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+  }
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void autonomousExit() {}
+
+  @Override
+  public void teleopInit() {
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void teleopExit() {}
+
+  @Override
+  public void testInit() {
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void testExit() {}
+}
---
+++ src/main/java/frc/robot/TargetAngle.java
@@ -0,0 +1,75 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
+/** Add your docs here. */
+public class TargetAngle {
+    // BEGIN Class Data
+    private Rotation2d m_targetAngle;
+    private double m_actualAngle;
+    private double m_hyp;
+    // END  Class Data
+
+    // BEGIN Class Constructors 
+    public TargetAngle(){
+
+    }
+    public TargetAngle(double _ang){
+        m_targetAngle = new Rotation2d(_ang);
+    }
+    public TargetAngle(double _x, double _y){
+        m_targetAngle = new Rotation2d(_x, _y);
+    }
+    // END Class Constructors
+
+    // BEGIN Class Methods
+    /** Set the target angle by giving a x and y value and creating a Rotation 2D object.
+     * A Rotation2d object is just a fancy class for holding an angle.
+     * 
+     * @param _x The x value that usually comes from the joystick
+     * @param _y The y value that usually comes from the joystick
+     */
+    public void setTargetAngle(double _x, double _y){
+        double midAngle = 45.0;
+        m_hyp = Math.hypot(_x, _y);
+        if(Math.abs(m_hyp) > 0.8){
+          m_actualAngle = Math.toDegrees(Math.atan2(_y,_x));
+          if(m_actualAngle >= -22.5 && m_actualAngle <= 22.5){
+            m_targetAngle = new Rotation2d(Math.toRadians(0));
+          }else if(m_actualAngle >= -67.5 && m_actualAngle < -22.5){
+            m_targetAngle = new Rotation2d(Math.toRadians(-midAngle));
+          }else if(m_actualAngle >= -112.5 && m_actualAngle < -67.5){
+            m_targetAngle = new Rotation2d(Math.toRadians(-90));
+          }else if(m_actualAngle >= -157.5 && m_actualAngle < -112.5){
+            m_targetAngle = new Rotation2d(Math.toRadians(-(180-midAngle)));
+          }else if((m_actualAngle >= 157.5 && m_actualAngle <= 180.0) || (m_actualAngle <= -157.5 && m_actualAngle > -179.99)){
+            m_targetAngle = new Rotation2d(Math.toRadians((180)));
+          }else if(m_actualAngle <= 67.5 && m_actualAngle > 22.5){
+            m_targetAngle = new Rotation2d(Math.toRadians(midAngle));
+          }else if(m_actualAngle <= 112.5 && m_actualAngle > 67.5){
+            m_targetAngle = new Rotation2d(Math.toRadians(90));
+          }else if(m_actualAngle <= 157.5 && m_actualAngle > 112.5){
+            m_targetAngle = new Rotation2d(Math.toRadians((180-midAngle)));
+          }else {
+            m_targetAngle = new Rotation2d();
+          }
+        }else {
+            m_targetAngle = new Rotation2d();
+        }
+        
+    }
+    public Rotation2d getTargetAngle(){
+        return m_targetAngle;
+    }
+    public double getActualAngle(){
+        return m_actualAngle;
+    }
+    public double getHyp(){
+        return m_hyp;
+    }
+    // END Class Constrcutors
+}
